4144e8f09ff457374b7e29f804e7ee9694ec71fa
Stephen Heckler <stephen@sheckler.info>
2019-07-04 11:32:41 -0500

    Misc comments/clarifications; credit where credit is due

--- 00-netplan_hook.sh
+++ 00-netplan_hook.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# The interface attached directly to the ONT port of the BGW210-700
 ATT_GATEWAY_INTERFACE="ens192f0"
 
 if [[ ${IFACE} = "br0" ]]
@@ -8,7 +9,7 @@ then
   echo -n 8 > /sys/class/net/br0/bridge/group_fwd_mask
 elif [[ ${IFACE} = "${ATT_GATEWAY_INTERFACE}" ]]
 then
-  # Once the interface of the BGW210-700 comes up, block all non-802.1X traffic to and from it
+  # Once the interface of the BGW210-700 comes up, block all non-802.1X traffic to/from it
   ebtables -t filter -A FORWARD -i "${ATT_GATEWAY_INTERFACE}" --proto 802_1Q --vlan-encap 0x888e -j ACCEPT
   ebtables -t filter -A FORWARD -i "${ATT_GATEWAY_INTERFACE}" --proto 802_1Q -j DROP
   ebtables -t filter -A FORWARD -o "${ATT_GATEWAY_INTERFACE}" --proto 802_1Q --vlan-encap 0x888e -j ACCEPT
